3c4ab29cddb7b21b4d3ef4ffaf3387811c085ad8
arcgocode <114783798+ArchitVaksh@users.noreply.github.com>
2026-07-30 17:23:15 +0530

    Time: 7 ms (77.95%) | Memory: 12.3 MB (89.95%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,7 +1,4 @@
 class Solution(object):
     def isPalindrome(self, x):
-        n = str(x)[ : :-1]
-        if n == str(x) and x >= 0:
-            return True
-        else:
-            return False
+        s = str(x)
+        return s == s[::-1]

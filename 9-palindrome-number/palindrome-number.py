899bef070837769bbbcbd2b5d9a66a52b7bb2841
arcgocode <114783798+ArchitVaksh@users.noreply.github.com>
2026-07-30 17:06:13 +0530

    Time: 20 ms (16.92%) | Memory: 12.3 MB (55.00%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        nen = x
+        new_digit = ""
+        while nen > 0:
+            digit = nen%10
+            new_digit  += str(digit)
+            nen //= 10
+        if new_digit == str(x):
+            return True
+        elif x == 0:
+            return True
+        else:
+            return False

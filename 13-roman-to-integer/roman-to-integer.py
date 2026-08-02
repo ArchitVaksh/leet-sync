87be6fc925aa591c6f7cd0426868af0a209f0ede
arcgocode <114783798+ArchitVaksh@users.noreply.github.com>
2026-08-03 02:15:21 +0530

    Time: 7 ms (73.77%) | Memory: 12.5 MB (21.37%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,13 +1,48 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        values = {
-            'I': 1, 'V': 5, 'X': 10, 'L': 50,
-            'C': 100, 'D': 500, 'M': 1000
-        }
-        total = 0
-        for i in range(len(s)):
-            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
-                total -= values[s[i]]
-            else:
-                total += values[s[i]]
-        return total
+class Solution(object):
+    def romanToInt(self, s):
+        result = 0
+        i = 0
+        while i < len(s):
+            if s[i] == "I":
+                if i + 1 < len(s) and s[i+1] == "V":
+                    result += 4
+                    i += 2
+                elif i + 1 < len(s) and s[i+1] == "X":
+                    result += 9
+                    i += 2
+                else:
+                    result += 1
+                    i += 1
+            elif s[i] == "V":
+                    result += 5
+                    i += 1
+            elif s[i] == "X":
+                if i + 1 < len(s) and s[i+1] == "L":
+                    result += 40
+                    i += 2
+                elif i + 1 < len(s) and s[i+1] == "C":
+                    result += 90
+                    i += 2
+                else:
+                    result += 10
+                    i += 1
+            elif s[i] == "L":
+                result += 50
+                i += 1
+            elif s[i] == "C":
+                if i + 1 < len(s) and s[i+1] == "D":
+                    result += 400
+                    i += 2
+                elif i + 1 < len(s) and s[i+1] == "M":
+                    result += 900
+                    i += 2
+                else:
+                    result += 100
+                    i += 1
+            elif s[i] == "D":
+                    result += 500
+                    i += 1
+            elif s[i] == "M":
+                    result += 1000
+                    i += 1
+        return result

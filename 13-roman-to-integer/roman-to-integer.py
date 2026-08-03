a1778b4ff1b73adcc3e1591fa7b2bc6b097d9934
arcgocode <114783798+ArchitVaksh@users.noreply.github.com>
2026-08-03 13:37:31 +0530

    Time: 6 ms (78.04%) | Memory: 12.3 MB (57.55%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,48 +1,22 @@
 class Solution(object):
     def romanToInt(self, s):
         result = 0
-        i = 0
-        while i < len(s):
-            if s[i] == "I":
-                if i + 1 < len(s) and s[i+1] == "V":
-                    result += 4
-                    i += 2
-                elif i + 1 < len(s) and s[i+1] == "X":
-                    result += 9
-                    i += 2
-                else:
-                    result += 1
-                    i += 1
-            elif s[i] == "V":
-                    result += 5
-                    i += 1
-            elif s[i] == "X":
-                if i + 1 < len(s) and s[i+1] == "L":
-                    result += 40
-                    i += 2
-                elif i + 1 < len(s) and s[i+1] == "C":
-                    result += 90
-                    i += 2
-                else:
-                    result += 10
-                    i += 1
-            elif s[i] == "L":
-                result += 50
-                i += 1
-            elif s[i] == "C":
-                if i + 1 < len(s) and s[i+1] == "D":
-                    result += 400
-                    i += 2
-                elif i + 1 < len(s) and s[i+1] == "M":
-                    result += 900
-                    i += 2
-                else:
-                    result += 100
-                    i += 1
-            elif s[i] == "D":
-                    result += 500
-                    i += 1
-            elif s[i] == "M":
-                    result += 1000
-                    i += 1
+        i = len(s)-1
+        romans = {"I":1,
+              "V":5,
+              "X":10,
+              "L":50,
+              "C":100,
+              "D":500,
+              "M":1000}
+        while i > 0:
+            if romans[s[i]] > romans[s[i-1]]:
+                result += romans[s[i]]
+                result -= romans[s[i-1]]
+                i -= 2
+            else:
+                result += romans[s[i]]
+                i -= 1
+        if i == 0:
+            result += romans[s[i]]
         return result

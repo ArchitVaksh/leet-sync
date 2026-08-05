d4d11f29a3ab043c24d61532cc94114191b1fa2f
arcgocode <114783798+ArchitVaksh@users.noreply.github.com>
2026-08-06 03:02:30 +0530

    Time: 3 ms (26.20%) | Memory: 12.5 MB (33.89%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,14 +1,20 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        if not strs:
-            return ""
-
-        prefix = strs[0]
-
-        for word in strs[1:]:
-            while word[:len(prefix)] != prefix:
-                prefix = prefix[:-1]
-                if not prefix:
-                    return ""
-
-        return prefix
+        result = ""
+        index = 0
+        while True:
+            current = ""
+            for word in strs:
+                if index >= len(word):
+                    return result
+                current += word[index]
+            flag = True
+            for i in range(1, len(current)):
+                if current[i] != current[0]:
+                    flag = False
+                    break
+            if flag:
+                result += current[0]
+                index += 1
+            else:
+                return result
